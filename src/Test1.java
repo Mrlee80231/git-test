e37f6d3b40fc0659b29ef8dbe290c0cf3c1ebdcd
Mrlee80231 <52661207+Mrlee80231@users.noreply.github.com>
2020-04-25 22:26:55 +0800

    Update Test1.java
    aaaa

--- src/Test1.java
+++ src/Test1.java
@@ -8,11 +8,7 @@
 public class Test1 {
     public static void main(String[] args) {
 
-        System.out.println ("我是修改后的Test1");
-        System.out.println ("我是修改后的Test1");
-        System.out.println ("我是修改后的Test1");
-        System.out.println ("我是修改后的Test1");
-        System.out.println ("我是修改后的Test1");
+       
         System.out.println ("我是公司另外一个开发者在dev分支提交的代码");
 
     }
