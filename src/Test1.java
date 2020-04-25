4238c39cd85ba909d320c6524c92cd7ba3059dca
Mr_lee <ltt30660539@qq.com>
2020-04-25 21:54:24 +0800

    master

--- src/Test1.java
+++ src/Test1.java
@@ -8,6 +8,8 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println ("我是master");
+        System.out.println ("我是master");
+        System.out.println ("我是mas        ter");
 
     }
 
